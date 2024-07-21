4d12a8edc0b6a05e7ad1b3d0b72ba760e0215e9f
goto2351 <gto9682@yahoo.co.jp>
2024-07-21 17:02:31 +0900

    プロジェクト ファイルを追加します。

---
+++ rasterizer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace rasterizer
+{
+    /// <summary>
+    /// エントリーポイント
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world");
+        }
+    }
+}
