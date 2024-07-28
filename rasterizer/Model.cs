21cede3753544e5b6d98e3b55c5138bbec2761d1
goto2351 <gto9682@yahoo.co.jp>
2024-07-28 18:35:00 +0900

    objファイルの読み込み結果からモデルデータを作る

---
+++ rasterizer/Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rasterizer
+{
+    /// <summary>
+    /// Objファイルから読み込んだ3Dモデル
+    /// </summary>
+    internal class Model : IModel
+    {
+        /// <summary>
+        /// 頂点
+        /// </summary>
+        public List<Point3D> VertexList => _vertexList;
+        private List<Point3D> _vertexList;
+
+        public Model(List<Point3D> vertexList)
+        {
+            _vertexList = vertexList;
+        }
+    }
+
+    internal class NullModel : IModel
+    {
+        public List<Point3D> VertexList { get
+            {
+                if (_vertexList == null)
+                {
+                    _vertexList = new List<Point3D>();
+                }
+                return _vertexList;
+            }
+        }
+        private List<Point3D> _vertexList = null;
+    }
+}
--- rasterizer/ObjFileLoader.cs
+++ rasterizer/ObjFileLoader.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ObjLoader;
 using ObjLoader.Loader.Data.DataStore;
+using ObjLoader.Loader.Data.VertexData;
 using ObjLoader.Loader.Loaders;
 
 namespace rasterizer
@@ -15,7 +16,7 @@ namespace rasterizer
     /// </summary>
     internal class ObjFileLoader
     {
-        public static void Load(string path)
+        public static IModel Load(string path)
         {
             // ObjLoaderインスタンスを作成
             var objLoaderFactory = new ObjLoaderFactory();
@@ -26,20 +27,58 @@ namespace rasterizer
             {
                 using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
                 {
-                    var res = objLoader.Load(fileStream);
-                    Console.WriteLine(res.ToString());
+                    var loadResult = objLoader.Load(fileStream);
+
+                    // 読み込んだ結果からModelを生成
+                    return CreateModelData(loadResult);
                 }
             }
             catch (FileNotFoundException ex)
             {
                 Console.WriteLine($"ファイルが見つかりませんでした path: {path}");
                 Console.WriteLine(ex.ToString());
+                return new NullModel();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"ファイルが読み込めませんでした path: {path}");
                 Console.WriteLine(ex.ToString());
+                return new NullModel();
             }
+        }
+
+        /// <summary>
+        /// ObjLoaderの読み込み結果からModelを生成する
+        /// </summary>
+        /// <param name="loadResult"></param>
+        /// <returns></returns>
+        private static IModel CreateModelData(LoadResult loadResult)
+        {
+            // 頂点情報が無ければNullオブジェクトを返す
+            if (loadResult == null || loadResult.Vertices.Count == 0)
+            {
+                return new NullModel();
+            }
+
+            return new Model(CreatePointList(loadResult.Vertices));
+        }
+
+        /// <summary>
+        /// ObjLoaderで読み込んだ頂点データをPoint3Dのリストに変換する
+        /// </summary>
+        /// <param name="vertexList"></param>
+        /// <returns></returns>
+        private static List<Point3D> CreatePointList(IList<Vertex> vertexList)
+        {
+            var pointList = new List<Point3D>();
+
+            foreach (var vertex in vertexList)
+            {
+                var point = new Point3D(vertex.X, vertex.Y, vertex.Z);
+                pointList.Add(point);
+            }
+
+            return pointList;
         }
     }
 }
