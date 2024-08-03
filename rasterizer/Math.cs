b5fc09b51796c68d974fbb4cd68951d9ddae64ad
goto2351 <gto9682@yahoo.co.jp>
2024-08-04 00:32:29 +0900

    ベクトル操作の処理を追加

---
+++ rasterizer/Math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rasterizer
+{
+    internal class Math
+    {
+        /// <summary>
+        /// ベクトルの内積
+        /// </summary>
+        /// <param name="vec1"></param>
+        /// <param name="vec2"></param>
+        /// <returns></returns>
+        public static float Dot(Vector3 vec1,  Vector3 vec2)
+        {
+            return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+        }
+
+        /// <summary>
+        /// ベクトルの外積(vec1 x vec2)
+        /// </summary>
+        /// <param name="vec1"></param>
+        /// <param name="vec2"></param>
+        /// <returns></returns>
+        public static Vector3 Cross(Vector3 vec1, Vector3 vec2)
+        {
+            var x = vec1.y * vec2.z - vec2.y * vec1.z;
+            var y = vec1.z * vec2.x - vec2.z * vec1.x;
+            var z = vec1.x * vec2.y - vec2.x * vec1.y;
+
+            return new Vector3(x, y, z);
+        }
+    }
+}
--- rasterizer/Vector3.cs
+++ rasterizer/Vector3.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 namespace rasterizer
 {
     /// <summary>
-    /// 3Dモデルの頂点
+    ///　3次元ベクトル
     /// </summary>
     internal class Vector3
     {
         public float x { get; }
         public float y { get; }
         public float z { get; }
+
+        /// <summary>
+        /// ベクトルのノルム
+        /// </summary>
+        public float Norm { 
+            get
+            {
+                return MathF.Sqrt(x * x + y * y + z * z);
+            }
+        }
 
         public Vector3(float x, float y, float z)
         {
@@ -22,9 +32,32 @@ namespace rasterizer
             this.z = z;
         }
 
+        /// <summary>
+        /// 正規化したベクトルを取得する
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 Normalize()
+        {
+            var x = this.x / this.Norm;
+            var y = this.y / this.Norm;
+            var z = this.z / this.Norm;
+
+            return new Vector3(x, y, z);
+        }
+
         public override string ToString()
         {
             return $"x: {x} y: {y}, z: {z}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType()) return false;
+
+            Vector3 vec = obj as Vector3;
+            if (vec == null) return false;
+
+            return this.x == vec.x && this.y == vec.y && this.z == vec.z;
         }
     }
 }
