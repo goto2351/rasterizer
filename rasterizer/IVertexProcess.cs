a97193c0f0708345555ef2fb4f76728fbba81d36
goto2351 <gto9682@yahoo.co.jp>
2024-08-04 14:19:52 +0900

    頂点処理の枠を作成

---
+++ rasterizer/IVertexProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rasterizer
+{
+    /// <summary>
+    /// 頂点処理のインターフェース
+    /// </summary>
+    public interface IVertexProcess
+    {
+        public List<Vector3> Transform(List<Vector3> vertexList);
+    }
+}
---
+++ rasterizer/VertexTransformer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rasterizer
+{
+    /// <summary>
+    /// 頂点変換をするクラス
+    /// </summary>
+    public class VertexTransformer
+    {
+    }
+}
