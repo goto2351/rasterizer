f3ea96e357e85b87cca0c38de7bca9d2962da54a
goto2351 <gto9682@yahoo.co.jp>
2024-08-04 18:20:54 +0900

    ビューポート変換の処理を追加

--- rasterizer/VertexTransformer.cs
+++ rasterizer/VertexTransformer.cs
@@ -20,7 +20,7 @@ namespace rasterizer
             _vertexList = vertexList;
         }
 
-        public void Transform()
+        public List<Pixel> Transform()
         {
             var viewingTransformation = new ViewingTransformation(_camera);
             var cameraCoordinateVertex = viewingTransformation.Transform(_vertexList);
@@ -29,6 +29,34 @@ namespace rasterizer
             {
                 Console.WriteLine($"{_vertexList[i]} → {cameraCoordinateVertex[i]}");
             }
+
+            var projectionTransformation = new ProjectionTransformation(_camera);
+            var deviceCoordinateVertex = projectionTransformation.Transform(cameraCoordinateVertex);
+            Console.WriteLine("頂点処理: 投影変換");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{cameraCoordinateVertex[i]} → {deviceCoordinateVertex[i]}");
+            }
+
+            // ビューポート変換
+            var harfScreenWidth = Program.SCREEN_WIDTH / 2f;
+            var harfScreenHeight = Program.SCREEN_HEIGHT / 2f;
+            var viewPortCoordinateVertex = new List<Vector3>();
+            foreach(var vertice in deviceCoordinateVertex)
+            {
+                viewPortCoordinateVertex.Add(new Vector3(
+                    x: vertice.x * harfScreenWidth + harfScreenWidth,
+                    y: -vertice.y * harfScreenHeight + harfScreenHeight,
+                    z: vertice.z * 0.5f + 0.5f));
+            }
+
+            Console.WriteLine("頂点処理: ビューポート変換");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{deviceCoordinateVertex[i]} → {viewPortCoordinateVertex[i]}");
+            }
+
+            return viewPortCoordinateVertex.Select(v => new Pixel((int)v.x, (int)v.y, Color.White)).ToList();
         }
     }
 }
